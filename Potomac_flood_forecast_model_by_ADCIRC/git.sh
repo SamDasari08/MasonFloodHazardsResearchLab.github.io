56b503c0b49b6d556588974c7bcdfc50dc0eafcc
admin <fhrl@Poseidon.vsnet.gmu.edu>
2017-05-24 11:00:09 -0400

    testing

--- Potomac_flood_forecast_model_by_ADCIRC/git.sh
+++ Potomac_flood_forecast_model_by_ADCIRC/git.sh
@@ -1,7 +1,7 @@
 #bin/bash!
 
 #ssh -p 22 fhrl@Poseidon.vsnet.gmu.edu
-#SERVER="fhrl@Poseidon.vsnet.gmu.edu"
+SERVER="akhalid6@gmu.edu"
 GITDIR=/home/fhrl/Documents/PotomicTidalForecastModel/RESULTS/PLOTS/MasonFloodHazardsResearchLab.github.io/Potomac_flood_forecast_model_by_ADCIRC
 
 
